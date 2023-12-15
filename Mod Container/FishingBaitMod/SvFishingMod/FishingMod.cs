db89a92a9e414e5a6ca182b8ee19d6e1f752868b
Moose <mustafaelfayoumi@hotmail.com>
2023-12-15 18:06:28 -0500

    Added Bait Objects and Odds Changes

---
+++ Mod Container/FishingBaitMod/SvFishingMod/FishingMod.cs
@@ -0,0 +1,56 @@
+﻿using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Menus;
+using StardewValley.Tools;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace SvFishingMod
+{
+    public sealed partial class FishingMod : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            Settings.HelperInstance = helper;
+            Settings.MonitorInstance = Monitor;
+            Settings.ConfigFilePath = "svfishmod.json";
+            Settings.LoadFromFile();
+
+            helper.ConsoleCommands.Add("sv_fishing_debug", "Enables or disables Debug mode on the SvFishingMod.\nUsage: sv_fishing_debug 0|1", HandleCommand);
+            helper.ConsoleCommands.Add("sv_fishing_enabled", "Enables or disables SvFishingMod.\nUsage: sv_fishing_enabled 0|1", HandleCommand);
+            helper.ConsoleCommands.Add("sv_fishing_setfish", "Forces the next fishing event to give a fish with the specified id.\nUsage: sv_fishing_setfish <fish_id>\nUse sv_fishing_search to get the id of a given fish by name.\nUse -1 as the fish id to restore original game functionality.", HandleCommand);
+       }
+
+        private void HandleCommand(string command, string[] args)
+        {
+            if (string.Equals(command, "sv_fishing_enabled", StringComparison.OrdinalIgnoreCase))
+            {
+                if (args != null && args.Length > 0)
+                {
+                    if (string.Equals(args[0].Trim(), "1", StringComparison.Ordinal))
+                        Settings.Local.DisableMod = false;
+                    else if (string.Equals(args[0].Trim(), "0", StringComparison.Ordinal))
+                        Settings.Local.DisableMod = true;
+                }
+                return;
+            }
+
+            if (string.Equals(command, "sv_fishing_setfish", StringComparison.OrdinalIgnoreCase))
+            {
+                if (args != null && args.Length > 0)
+                {
+                    if (int.TryParse(args[0].Trim(), out int fishId))
+                    {
+                        Settings.Local.OverrideFishType = fishId;
+                        Monitor.Log(string.Format("Done. The next reeled fish will be {0}", fishId), LogLevel.Info);
+                    }
+                    else
+                        Monitor.Log("Invalid fish id specified.", LogLevel.Info);
+                }
+                return;
+            }
+        }
+    }
+}
---
+++ Mod Container/FishingBaitMod/SvFishingMod/Settings.cs
@@ -0,0 +1,129 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+
+namespace SvFishingMod
+{
+    [DataContract]
+    public class Settings
+    {
+        private static Settings _localInstance = null;
+        private int _overrideFishType = -1;
+
+        private Settings() { }
+
+        public static Settings DefaultEnabled
+        {
+            get
+            {
+                Settings output = new Settings()
+                {
+                    DisableMod = false,
+                };
+
+
+                return output;
+            }
+        }
+
+        public static Settings DefaultDisabled
+        {
+            get
+            {
+                Settings output = new Settings()
+                {
+                    DisableMod = true,
+                };
+
+                return output;
+            }
+        }
+
+        public static string ConfigFilePath { get; set; } = null;
+        public static IModHelper HelperInstance { get; set; } = null;
+        public static IMonitor MonitorInstance { get; set; } = null;
+
+        public static Settings Local
+        {
+            get
+            {
+                if (_localInstance == null)
+                {
+                    if (string.IsNullOrWhiteSpace(ConfigFilePath))
+                        _localInstance = Settings.DefaultEnabled;
+                    else
+                        _localInstance = LoadFromFile(ConfigFilePath);
+                }
+
+                return _localInstance;
+            }
+            set
+            {
+                _localInstance = value;
+            }
+        }
+        [DataMember] public bool DisableMod { get; set; } = false;
+
+        [DataMember]
+        public int OverrideFishType
+        {
+            get
+            {
+                if (_overrideFishType == -1)
+                    return -1;
+                if (_overrideFishType < 128)
+                    return 128;
+
+                return _overrideFishType;
+            }
+            set
+            {
+                _overrideFishType = value;
+            }
+        }
+
+        public static Settings LoadFromFile()
+        {
+            if (HelperInstance == null)
+                throw new NullReferenceException("No SMAPI Mod Helper defined before loading settings file.");
+
+            return LoadFromFile(ConfigFilePath);
+        }
+
+        public static Settings LoadFromFile(string filename)
+        {
+            Settings output;
+
+            try
+            {
+                output = HelperInstance.Data.ReadJsonFile<Settings>(ConfigFilePath);
+                if (output == null)
+                    output = new Settings();
+                output.SaveToFile(filename);
+                MonitorInstance.Log(string.Format("Settings loaded using SMAPI from {0}", filename), LogLevel.Trace);
+            }
+            catch (Exception ex)
+            {
+                if (MonitorInstance != null)
+                    MonitorInstance.Log(string.Format("[SvFishingMod] Unable to load settings from specified filename {0}", filename, ex.GetType().Name, ex.Message), LogLevel.Error);
+                output = new Settings(); // Load defaults
+            }
+
+            return output;
+        }
+
+        public void SaveToFile()
+        {
+            SaveToFile(ConfigFilePath);
+        }
+
+        public void SaveToFile(string filename)
+        {
+            if (HelperInstance == null)
+                throw new NullReferenceException("No SMAPI Mod Helper defined before saving settings file.");
+
+            HelperInstance.Data.WriteJsonFile<Settings>(filename, this);
+        }
+    }
+}
